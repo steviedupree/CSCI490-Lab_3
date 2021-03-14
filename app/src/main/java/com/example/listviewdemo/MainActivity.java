289f73c44f6762973fc076f6fc5a0c5324fc3a24
steviedupree <dupreesc@g.cofc.edu>
2021-03-14 13:19:39 -0400

    add listview

---
+++ app/src/main/java/com/example/listviewdemo/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.listviewdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
